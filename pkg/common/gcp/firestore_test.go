7f49775ad77c91bf8cdef52163881bb2fbebc19e
Nils Lamot <nilslamot@gmail.com>
2021-05-17 00:32:50 +0200

    Fix unit test for build on google cloud

--- pkg/common/gcp/firestore_test.go
+++ pkg/common/gcp/firestore_test.go
@@ -1,6 +1,7 @@
 package gcp_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,6 +10,7 @@ import (
 
 
 func TestFirestoreClientShutsDownApplicationIfNotConnectedProperly(t *testing.T) {
+	os.Setenv("GCP_PROJECT", "INVALID")
 	_, err := gcp.ProvideFirestoreClient(gcp.GetConfiguration())
 	assert.EqualError(t, err, "google: could not find default credentials. See https://developers.google.com/accounts/docs/application-default-credentials for more information.")
 }
