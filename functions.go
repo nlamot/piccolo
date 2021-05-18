c5019a4829c068a7e6e7e671e4cd829c1e2fb0ad
Nils Lamot <nilslamot@gmail.com>
2021-05-18 23:11:38 +0200

    Run container creastion once, speeding up functions

--- functions.go
+++ functions.go
@@ -2,14 +2,20 @@ package planner
 
 import (
 	"net/http"
+	"sync"
 
+	"go.uber.org/dig"
 	"piccolo.com/planner/pkg/genetic/population"
 )
 
+var container *dig.Container
+var containerOnce sync.Once
 
 // GeneratePopulation generates the population for the genetic internship planner
 func GeneratePopulation(w http.ResponseWriter, r *http.Request) {
-	container := population.NewContainer()
+	containerOnce.Do(func() {
+		container = population.NewContainer()
+	})
 
 	err := container.Invoke(func(handler population.PopulationController) {
 		handler.Generate(w, r)
